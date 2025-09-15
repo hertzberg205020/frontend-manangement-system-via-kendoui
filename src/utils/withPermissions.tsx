a4800288a7816c21022356f7e0cac050102af769
Graham <u8360182@gmail.com>
2025-09-15 10:56:27 +0800

    feat(withPermissions): 新增權限檢查高階組件
    新增一個高階組件以檢查用戶的權限，確保只有擁有所需權限的用戶才能訪問特定組件。

---
+++ src/utils/withPermissions.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+interface WithPermissionsProps {
+  userPermissions: string[];
+}
+
+function withPermissions<P>(requiredPermissions: string[]) {
+  return (Component: React.ComponentType<Omit<P, 'userPermissions'>>) => {
+    return (props: P & WithPermissionsProps) => {
+      const { userPermissions, ...restProps } = props;
+      const hasPermission = requiredPermissions.every(item => userPermissions.includes(item));
+      if (!hasPermission) return null;
+      return <Component {...(restProps as Omit<P, 'userPermissions'>)} />;
+    };
+  };
+}
+
+export default withPermissions;
