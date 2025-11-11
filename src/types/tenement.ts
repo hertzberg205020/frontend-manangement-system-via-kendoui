d101b2cd6962de3c11b6356fe99813f245bce1cc
graham <u8360182@gmail.com>
2025-11-11 14:40:15 +0800

    style(tenement): 調整 CreateTenementDataType 的格式

--- src/types/tenement.ts
+++ src/types/tenement.ts
@@ -10,7 +10,9 @@ interface TenementDataType {
   managementFee: string;
 }
 
-interface CreateTenementDataType extends Omit<TenementDataType, 'id' | 'vacancyRate' | 'propertyFee'> {
+interface CreateTenementDataType extends Omit<
+  TenementDataType, 'id' | 'vacancyRate' | 'propertyFee'
+> {
   id?: string; // 在新增時，ID 可以是可選的
   vacancyRate?: number; // 在新增時，vacancyRate 可以是可選的
   propertyFee?: string; // 在新增時，propertyFee 可以是可選的
